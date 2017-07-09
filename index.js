8e86582e54a4e19979af7919d6bf4e90adbaf39f
Nam Tran <tranngocnam97@yahoo.com>
2017-07-09 01:26:40 -0700

    Refactoring

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var Alexa = require('alexa-sdk');
 var APP_ID = undefined; // TODO replace with your app ID (OPTIONAL).
-var recipes = require('./recipes');
+var measurements = require('./measurements');
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
@@ -23,28 +23,28 @@ var handlers = {
         this.attributes['repromptSpeech'] = this.t("WELCOME_REPROMPT");
         this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
     },
-    'RecipeIntent': function () {
-        var itemSlot = this.event.request.intent.slots.Item;
-        var itemName;
-        if (itemSlot && itemSlot.value) {
-            itemName = itemSlot.value.toLowerCase();
+    'MeasurementIntent': function () {
+        var measurementSlot = this.event.request.intent.slots.Measurement;
+        var measurementName;
+        if (measurementSlot && measurementSlot.value) {
+            measurementName = measurementSlot.value.toLowerCase();
         }
 
-        var cardTitle = this.t("DISPLAY_CARD_TITLE", this.t("SKILL_NAME"), itemName);
-        var recipes = this.t("RECIPES");
-        var recipe = recipes[itemName];
+        var cardTitle = this.t("DISPLAY_CARD_TITLE", this.t("SKILL_NAME"), measurementName);
+        var measurements = this.t("MEASUREMENTS");
+        var measurement = measurements[measurementName];
 
-        if (recipe) {
-            this.attributes['speechOutput'] = recipe;
-            this.attributes['repromptSpeech'] = this.t("RECIPE_REPEAT_MESSAGE");
-            this.emit(':tellWithCard', recipe, this.attributes['repromptSpeech'], cardTitle, recipe);
+        if (measurement) {
+            this.attributes['speechOutput'] = measurement;
+            this.attributes['repromptSpeech'] = this.t("MEASUREMENT_REPEAT_MESSAGE");
+            this.emit(':tellWithCard', measurement, this.attributes['repromptSpeech'], cardTitle, measurement);
         } else {
-            var speechOutput = this.t("RECIPE_NOT_FOUND_MESSAGE");
-            var repromptSpeech = this.t("RECIPE_NOT_FOUND_REPROMPT");
-            if (itemName) {
-                speechOutput += this.t("RECIPE_NOT_FOUND_WITH_ITEM_NAME", itemName);
+            var speechOutput = this.t("MEASUREMENT_NOT_FOUND_MESSAGE");
+            var repromptSpeech = this.t("MEASUREMENT_NOT_FOUND_REPROMPT");
+            if (measurementName) {
+                speechOutput += this.t("MEASUREMENT_NOT_FOUND_WITH_ITEM_NAME", measurementName);
             } else {
-                speechOutput += this.t("RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME");
+                speechOutput += this.t("MEASUREMENT_NOT_FOUND_WITHOUT_ITEM_NAME");
             }
             speechOutput += repromptSpeech;
 
@@ -81,48 +81,31 @@ var handlers = {
 var languageStrings = {
     "en": {
         "translation": {
-            "RECIPES": recipes.RECIPE_EN_US,
-            "SKILL_NAME": "Minecraft Helper",
-            "WELCOME_MESSAGE": "Welcome to %s. You can ask a question like, what\'s the recipe for a chest? ... Now, what can I help you with.",
+            "MEASUREMENTS": measurements.MEASUREMENT_EN_US,
+            "SKILL_NAME": "Sensor Helper",
+            "WELCOME_MESSAGE": "Welcome to %s. You can ask a question like, what\'s the measurement for a chest? ... Now, what can I help you with.",
             "WELCOME_REPROMPT": "For instructions on what you can say, please say help me.",
-            "DISPLAY_CARD_TITLE": "%s  - Recipe for %s.",
-            "HELP_MESSAGE": "You can ask questions such as, what\'s the recipe, or, you can say exit...Now, what can I help you with?",
-            "HELP_REPROMPT": "You can say things like, what\'s the recipe, or you can say exit...Now, what can I help you with?",
+            "DISPLAY_CARD_TITLE": "%s  - Measurement for %s.",
+            "HELP_MESSAGE": "You can ask questions such as, what\'s the measurement, or, you can say exit...Now, what can I help you with?",
+            "HELP_REPROMPT": "You can say things like, what\'s the measurement, or you can say exit...Now, what can I help you with?",
             "STOP_MESSAGE": "Goodbye!",
-            "RECIPE_REPEAT_MESSAGE": "Try saying repeat.",
-            "RECIPE_NOT_FOUND_MESSAGE": "I\'m sorry, I currently do not know ",
-            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "the recipe for %s. ",
-            "RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME": "that recipe. ",
-            "RECIPE_NOT_FOUND_REPROMPT": "What else can I help with?"
+            "MEASUREMENT_REPEAT_MESSAGE": "Try saying repeat.",
+            "MEASUREMENT_NOT_FOUND_MESSAGE": "I\'m sorry, I currently do not know ",
+            "MEASUREMENT_NOT_FOUND_WITH_ITEM_NAME": "the measurement for %s. ",
+            "MEASUREMENT_NOT_FOUND_WITHOUT_ITEM_NAME": "that measurement. ",
+            "MEASUREMENT_NOT_FOUND_REPROMPT": "What else can I help with?"
         }
     },
     "en-US": {
         "translation": {
-            "RECIPES" : recipes.RECIPE_EN_US,
-            "SKILL_NAME" : "American Minecraft Helper"
+            "MEASUREMENTS" : measurements.MEASUREMENT_EN_US,
+            "SKILL_NAME" : "Sensor Namer (US)"
         }
     },
     "en-GB": {
         "translation": {
-            "RECIPES": recipes.RECIPE_EN_GB,
-            "SKILL_NAME": "British Minecraft Helper"
-        }
-    },
-    "de": {
-        "translation": {
-            "RECIPES" : recipes.RECIPE_DE_DE,
-            "SKILL_NAME" : "Assistent für Minecraft in Deutsch",
-            "WELCOME_MESSAGE": "Willkommen bei %s. Du kannst beispielsweise die Frage stellen: Welche Rezepte gibt es für eine Truhe? ... Nun, womit kann ich dir helfen?",
-            "WELCOME_REPROMPT": "Wenn du wissen möchtest, was du sagen kannst, sag einfach „Hilf mir“.",
-            "DISPLAY_CARD_TITLE": "%s - Rezept für %s.",
-            "HELP_MESSAGE": "Du kannst beispielsweise Fragen stellen wie „Wie geht das Rezept für“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?",
-            "HELP_REPROMPT": "Du kannst beispielsweise Sachen sagen wie „Wie geht das Rezept für“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?",
-            "STOP_MESSAGE": "Auf Wiedersehen!",
-            "RECIPE_REPEAT_MESSAGE": "Sage einfach „Wiederholen“.",
-            "RECIPE_NOT_FOUND_MESSAGE": "Tut mir leid, ich kenne derzeit ",
-            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "das Rezept für %s nicht. ",
-            "RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME": "dieses Rezept nicht. ",
-            "RECIPE_NOT_FOUND_REPROMPT": "Womit kann ich dir sonst helfen?"
+            "MEASUREMENTS": measurements.MEASUREMENT_EN_US,
+            "SKILL_NAME": "Sensor Namer (GB)"
         }
     }
 };
